e4c86ff57554f4358141cc19e47701bf8d5304a6
Marshall Farrier <info@marshallfarrier.com>
2013-11-20 07:26:48 -0800

    Test uniq when string is input.

--- tst/uniq-test.js
+++ tst/uniq-test.js
@@ -3,7 +3,7 @@ var codeMelon = codeMelon || {};
 (function(_c) {
     _c.utils = _c.utils || {};
 
-    test( "dedupe test", function() {
+    test( "dedupe test on array", function() {
         var arr = ['cat', 'dog', 'man', 'cat', 'mouse', 'dog'],
             result = _c.utils.uniq(arr),
             i;
@@ -11,6 +11,16 @@ var codeMelon = codeMelon || {};
         strictEqual(result.length, arr.length - 2, "correct number of elements eliminated");
         for (i = 0; i < arr.length; i++) {
             ok(result.indexOf(arr[i]) > -1, "result contains all items in original array");
+        }
+    });
+    test( "dedupe test on string", function() {
+        var testcase = 'abcabcdef',
+            result = _c.utils.uniq(testcase),
+            i;
+
+        strictEqual(result.length, testcase.length - 3, "correct number of characters eliminated");
+        for (i = 0; i < testcase.length; i++) {
+            ok(result.indexOf(testcase[i]) > -1, "result contains all items in original string");
         }
     });
 })(codeMelon);
