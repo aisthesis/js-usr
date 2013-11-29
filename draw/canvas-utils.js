585ef55d81a0a5816fa64a508138fded34bc2050
Marshall Farrier <info@marshallfarrier.com>
2013-11-28 23:59:26 -0800

    Optional point parameter for windowToCanvas()

--- draw/canvas-utils.js
+++ draw/canvas-utils.js
@@ -10,9 +10,25 @@ var _c = _c || {};
     "use strict";
 
     _c.draw = _c.draw || {};
-    _c.draw.windowToCanvas = function(canvas, x, y) {
+    /**
+     * Convert window coordinates to canvas coordinates. Providing
+     * the optional point parameter improves performance if the
+     * desired point already exists.
+     * @param {Object} canvas - canvas relative to which coordinates
+     * are to be determined
+     * @param {Number} x - window x coordinate
+     * @param {Number} y - window y coordinate
+     * @param {Point} [point] - point in which to store result
+     * @returns {Point}
+     */
+    _c.draw.windowToCanvas = function(canvas, x, y, point) {
         var bbox = canvas.getBoundingClientRect();
 
+        if (point) {
+            point.x = x - bbox.left;
+            point.y = y - bbox.top;
+            return point;
+        }
         return new _c.draw.Point(x - bbox.left, y - bbox.top);
     }
 })(_c);
