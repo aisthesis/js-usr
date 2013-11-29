80788e8856b4a55a304223ea7dd173138d60c08a
Marshall Farrier <info@marshallfarrier.com>
2013-11-29 01:14:01 -0800

    Add function to get offset of an event.

--- draw/canvas-utils.js
+++ draw/canvas-utils.js
@@ -30,5 +30,37 @@ var _c = _c || {};
             return point;
         }
         return new _c.draw.Point(x - bbox.left, y - bbox.top);
+    },
+
+    /**
+     * Returns a vector specifying the offset of the given
+     * event from the given point on the given canvas.
+     * @param {Object} canvas - canvas on which offset is to be determined
+     * @param {Point} orig - point relative to which the offset is to be
+     * measured
+     * @param {Object} event - event whose position relative to the original
+     * point is to be determined
+     * @param {Point} [loc] - point that will be modified to show
+     * the location of the event relative to the canvas. Re-using this point 
+     * will improve performance if the function is called repeatedly, such as
+     * on any mousemove event.
+     * @param {Vector} [offset] - optional vector in which to store the result
+     * @returns {Vector}
+     */
+    _c.draw.getOffset = function(canvas, orig, event, loc, offset) {
+        var loc; 
+
+        if (loc) {
+            _c.draw.windowToCanvas(canvas, event.clientX, event.clientY, loc);
+        }
+        else {
+            loc = _c.draw.windowToCanvas(canvas, event.clientX, event.clientY);
+        }
+        if (offset) {
+            offset.x = loc.x - orig.x;
+            offset.y = loc.y - orig.y;
+            return offset;
+        }
+        return new _c.draw.Vector(loc.x - orig.x, loc.y - orig.y);
     }
 })(_c);
