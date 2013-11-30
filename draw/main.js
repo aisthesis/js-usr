bfc4c29dafe8c584627954d512184c636f1f437f
Marshall Farrier <info@marshallfarrier.com>
2013-11-30 09:09:48 -0800

    Generic module file to load all files in draw directory.

---
+++ draw/main.js
@@ -0,0 +1,10 @@
+define(function(require) {
+    var extend = require('../usr/extend'),
+        canvasUtils = require('../usr/draw/canvas-utils'),
+        highlights = require('../usr/draw/highlights'),
+        Point = require('../usr/draw/point'),
+        Shape = require('../usr/draw/shape'),
+        Circle = require('../usr/draw/circle'),
+        Rectangle = require('../usr/draw/rectangle'),
+        RegularPolygon = require('../usr/draw/regular-polygon');
+});
