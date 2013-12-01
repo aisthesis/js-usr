af069dbc9ffea21f900cc3a84a8d880884655e82
Marshall Farrier <info@marshallfarrier.com>
2013-12-01 11:33:45 -0800

    More flexible module declaration.

--- draw/main.js
+++ draw/main.js
@@ -1,10 +1,32 @@
-define(function(require) {
-    var extend = require('../usr/extend'),
-        canvasUtils = require('../usr/draw/canvas-utils'),
-        highlights = require('../usr/draw/highlights'),
-        Point = require('../usr/draw/point'),
-        Shape = require('../usr/draw/shape'),
-        Circle = require('../usr/draw/circle'),
-        Rectangle = require('../usr/draw/rectangle'),
-        RegularPolygon = require('../usr/draw/regular-polygon');
-});
+/**
+ * Paths are relative to the location from which
+ * require.js is called.
+ * To assemble the module, define the path variable
+ * _c.path.usr in main.js
+ * For example:
+ * var _c = _c || {};
+ * _c.path = _c.path || {};
+ * _c.path.usr = '../usr/';
+ */
+(function(path) {
+    var files = [
+        'extend',
+        'draw/canvas-utils',
+        'draw/highlights',
+        'draw/point',
+        'draw/shape',
+        'draw/circle',
+        'draw/rectangle',
+        'draw/regular-polygon'
+    ];
+
+    files.forEach(function(file, i) {
+        files[i] = path + file;
+    });
+    define(['require'].concat(files),
+        function(require) {
+            files.forEach(function(file) {
+                require(file);
+            });
+        });
+})(_c.path.usr);
